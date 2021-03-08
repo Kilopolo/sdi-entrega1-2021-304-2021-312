041df2d44a61627f46f5a3c038350f6158ffcb31
antsuarz <uo270543@uniovi.es>
2021-03-08 18:43:35 +0100

    Funciona por primera vez :)

--- MyWallapop/src/main/java/com/uniovi/services/RolesService.java
+++ MyWallapop/src/main/java/com/uniovi/services/RolesService.java
@@ -1,5 +1,13 @@
 package com.uniovi.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RolesService {
 
+	String[] roles = { "ROLE_ADMIN", "ROLE_USER" };
+
+	public String[] getRoles() {
+		return roles;
+	}
 }
