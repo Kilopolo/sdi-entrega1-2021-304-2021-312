cb8ae0e0fe9a30460f9c18e800b54f86e8a0e3f4
antsuarz <uo270543@uniovi.es>
2021-03-08 16:55:05 +0100

    Update User.java

--- MyWallapop/src/main/java/com/uniovi/entities/User.java
+++ MyWallapop/src/main/java/com/uniovi/entities/User.java
@@ -1,7 +1,12 @@
 package com.uniovi.entities;
 
 public class User {
+	
+	private String email;
 	private String name;
+	private String lastName;
+	private String password;
+	
 	public User() {}
 	
 	public User(String name) {
